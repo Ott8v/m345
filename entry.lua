c2abdd25a5ccd23b57eacb83560da515eb0c3e17
Federico Balducci <ott8v@proton.me>
2024-03-23 00:23:49 +0100

    Feature(entry.lua): add Options, InputProfiles

--- entry.lua
+++ entry.lua
@@ -1,14 +1,28 @@
-declare_plugin("M-345",
+local self_ID = "M-345"
+
+declare_plugin(self_ID,
 {
 displayName = _("M-345"),
 
 installed = true,
 dirName = current_mod_path,
 fileMenuName = _("M-345"),
+info = _("M-345 mod"),
 
 version = "1.0.0",
 state = "installed",
-encyclopedia_path = current_mod_path .. '/Encyclopedia'
+encyclopedia_path = current_mod_path .. '/Encyclopedia',
+InputProfiles = {
+    ["M-345"] = current_mod_path .. '/Input',
+},
+Options = {
+    {
+        name = _("M-345"),
+        nameId = "M-345",
+        dir = "Options",
+        CLSID = "{M-345 options}"
+    }
+}
 })
 
 plugin_done()
